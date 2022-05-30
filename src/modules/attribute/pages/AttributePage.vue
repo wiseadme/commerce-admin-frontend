ff72175bf9c9638a291ca71c876b9f7d895446f7
Anar <wiseadme@gmail.com>
2022-05-31 00:25:12 +0300

    correct

--- src/modules/attribute/pages/AttributePage.vue
+++ src/modules/attribute/pages/AttributePage.vue
@@ -1,7 +1,7 @@
 <script>
   import { defineComponent, ref, toRaw } from 'vue'
   // import { useAttributeService } from '../service/attribute.service'
-  import {clone} from '@shared/helpers'
+  import { clone } from '@shared/helpers'
 
   export default defineComponent({
     name: 'attribute-page',
@@ -15,9 +15,10 @@
         attributePattern.value = { key: '', value: '', meta: '' }
       }
 
-      const onCreate = (attribute) => {
-        attributes.value.push(clone(toRaw(attributePattern.value)))
-        console.log(attribute)
+      const onCreate = (validate) => {
+        validate().then(() => {
+          attributes.value.push(clone(toRaw(attributePattern.value)))
+        })
         // service.createAttribute(attribute)
       }
 
@@ -37,58 +38,63 @@
         xl="4"
         lg="6"
         md="12"
+        sm="12"
       >
-        <v-card
-          width="100%"
-          elevation="2"
-          color="#ffffff"
-        >
-          <v-card-title class="green--text">
-            <h3>Аттрибуты</h3>
-          </v-card-title>
-          <v-card-content>
-            <v-text-field
-              v-model="attributePattern.key"
-              label="Название"
-              color="#272727"
-            />
-            <v-text-field
-              v-model="attributePattern.value"
-              label="Значение"
-              color="#272727"
-            />
-            <v-text-field
-              v-model="attributePattern.meta"
-              label="Мета информация"
-              color="#272727"
-            />
-          </v-card-content>
-          <v-card-actions class="">
-            <v-button
-              elevation="2"
-              color="green"
-              @click="onCreate"
-            >
-              <v-icon
-                size="14"
+        <v-form v-slot="{validate}">
+          <v-card
+            width="100%"
+            elevation="2"
+            color="#ffffff"
+          >
+            <v-card-title class="green--text">
+              <h3>Аттрибуты</h3>
+            </v-card-title>
+            <v-card-content>
+              <v-text-field
+                v-model="attributePattern.key"
+                label="Название*"
+                color="#272727"
+                :rules="[val => !!val || 'Обязательное поле']"
+              />
+              <v-text-field
+                v-model="attributePattern.value"
+                label="Значение по умолчанию*"
+                color="#272727"
+                :rules="[val => !!val || 'Обязательное поле']"
+              />
+              <v-text-field
+                v-model="attributePattern.meta"
+                label="Мета информация"
+                color="#272727"
+              />
+            </v-card-content>
+            <v-card-actions class="">
+              <v-button
+                elevation="2"
+                color="green"
+                @click="onCreate(validate)"
               >
-                fas fa-plus
-              </v-icon>
-            </v-button>
-            <v-button
-              elevation="2"
-              color="error"
-              class="ml-2"
-              @click="clearForm"
-            >
-              <v-icon
-                size="14"
+                <v-icon
+                  size="14"
+                >
+                  fas fa-plus
+                </v-icon>
+              </v-button>
+              <v-button
+                elevation="2"
+                color="error"
+                class="ml-2"
+                @click="clearForm"
               >
-                fas fa-trash-alt
-              </v-icon>
-            </v-button>
-          </v-card-actions>
-        </v-card>
+                <v-icon
+                  size="14"
+                >
+                  fas fa-trash-alt
+                </v-icon>
+              </v-button>
+            </v-card-actions>
+          </v-card>
+        </v-form>
       </v-col>
       <v-col
         xl="8"
@@ -96,19 +102,19 @@
         md="12"
         sm="12"
       >
-        <v-list>
-          <v-list-item
-            v-for="it in attributes"
-            :key="it.key"
-            class="elevation-2 my-1"
-          >
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ it.key }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <div
+          v-for="it in attributes"
+          :key="it.key"
+          class="d-flex justify-start align-center elevation-2 my-1 py-4 px-3 white"
+        >
+          <span>
+            {{ it.key }}
+          </span>
+          <v-spacer></v-spacer>
+          <v-icon clickable>
+            fas fa-times
+          </v-icon>
+        </div>
       </v-col>
     </v-row>
   </v-layout>
